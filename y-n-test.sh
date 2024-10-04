9475caf0ff2a900fd008e5045b682ed3034f8e6a
deadplev-ai <ethan.sanders@videoanalysis.tv>
2024-10-04 18:22:22 +0100

    Update y-n-test.sh

--- y-n-test.sh
+++ y-n-test.sh
@@ -3,11 +3,22 @@ function yes_or_no {
         read -p "$* [y/n]: " yn
         case $yn in
             [Yy]*) return 0  ;;  
-            [Nn]*) echo "Aborted" ; return  1 ;;
+            [Nn]*) return  1 ;;
         esac
     done
 }
 
-yes_or_no "Install Fail to ban? (Can cause issues with Mail)" && echo "Install #1"
+yes_or_no "Install Fail2ban? (Anti SSH Brute Force)" && apt-get install fail2ban -y
+# MENTION MAIL VERSION FOR MAIL SERVER
 
-yes_or_no "Install ProxyManager?" && echo "Install #2"
+yes_or_no "Install ClamAV? (File AntiVirus)" && echo "Install #2. AUTO SCAN, AUTO UPDATE SIGNATURES. DO POST UFW SO YOU CAN ADD RULES"
+
+yes_or_no "Disable Unused Filesystems? (Reduce Attack Vectors)" && echo "Install #2"
+
+yes_or_no "Install Advanced Intrusion Detection (AID)?" && echo "Install #3"
+
+yes_or_no "Harden Kernel Security Paramaters?" && echo "Install #4"
+
+yes_or_no "Disable Core Dumps? (!!No More Crash Logs!!)" && echo "Install #5"
+
+#App Armor config in future
