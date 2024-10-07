b7b8af02d2bf58d8e6444b6c8b3b354d7bd2f96f
Ethan Sanders <ethan.sanders@videoanalysis.tv>
2024-10-07 10:14:42 +0100

    Add y/n Function

--- setup_user.sh
+++ setup_user.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) return  1 ;;
+        esac
+    done
+}
+
 cd ~
 mkdir ~/.ssh
 chmod 700 ~/.ssh
