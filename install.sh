311c8e06a98bf085afe822810f7413729454909d
Ethan Sanders <ethan.sanders@videoanalysis.tv>
2024-10-07 09:28:13 +0100

    Auto Cleanup

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ read -p "New Username: " desired_user && adduser $desired_user && usermod -aG su
 echo "$desired_user ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$desired_user && \
 cp setup_user.sh /home/$desired_user/ && chown $desired_user:$desired_user /home/$desired_user/setup_user.sh && \
 su - $desired_user -c "bash ~/setup_user.sh"
+
+# Auto Cleanup (Remove if break)
+rm /ASS_Auto-Server-Secure/*
+rmdir /ASS_Auto-Server-Secure
