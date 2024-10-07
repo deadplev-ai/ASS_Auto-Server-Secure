3fd67b8e916663f54a137b1ceb257613f04df5cd
Ethan Sanders <ethan.sanders@videoanalysis.tv>
2024-10-07 09:25:24 +0100

    Update y-n-test.sh

--- y-n-test.sh
+++ y-n-test.sh
@@ -8,17 +8,25 @@ function yes_or_no {
     done
 }
 
-yes_or_no "Install Fail2ban? (Anti SSH Brute Force)" && apt-get install fail2ban -y
-# MENTION MAIL VERSION FOR MAIL SERVER
+# Fail2Ban + Email | Anti SSH Brute Force
+yes_or_no "Install Fail2ban? (Anti SSH Brute Force)" && apt-get install fail2ban -y && yes_or_no "Notify by EMail on Security Event?" && echo "test"
 
+# ClamAV + Auto File Scan | Antivirus
 yes_or_no "Install ClamAV? (File AntiVirus)" && echo "Install #2. AUTO SCAN, AUTO UPDATE SIGNATURES. DO POST UFW SO YOU CAN ADD RULES"
 
+# Disable Unused Filesystems
 yes_or_no "Disable Unused Filesystems? (Reduce Attack Vectors)" && echo "Install #2"
 
+# Intrusion Detection EMails
 yes_or_no "Install Advanced Intrusion Detection (AID)?" && echo "Install #3"
 
+# Harden Kernel Security
 yes_or_no "Harden Kernel Security Paramaters?" && echo "Install #4"
 
+# Disable Core Dumps / Logs
 yes_or_no "Disable Core Dumps? (!!No More Crash Logs!!)" && echo "Install #5"
+
+# SSH 2FA
+yes_or_no "Install SSH 2FA?" && echo "aight"
 
 #App Armor config in future
