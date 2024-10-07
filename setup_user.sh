88e81804f69e210665d8812ee2af1ec84258e66c
Ethan Sanders <ethan.sanders@videoanalysis.tv>
2024-10-07 11:38:28 +0100

    Y/N 2FA

--- setup_user.sh
+++ setup_user.sh
@@ -31,7 +31,26 @@ sudo ufw status
 yes_or_no "Enforce Password Complexity Requirements? (For future password updates)" && sudo apt -y install libpam-pwquality cracklib-runtime && sed -i 'password   requisite   pam_pwquality.so retry=3/password   requisite   pam_pwquality.so minlen=8 ucredit=-1 lcredit=-1 dcredit=-1 gecoscheck=1 reject_username enforce_for_root' /etc/pam.d/common-password && echo && echo "*****************************" && echo "MINIMUM PASSWORD REQUIREMENTS" && echo "Min Length: 8" && echo "Uppercase: 1" && echo "Lowercase: 1" && echo "Numbers: 1" && echo "CANNOT BE USERNAME | CANNOT CONTAIN GECOS INFO" && echo "*****************************" && echo
 
 # Fail2Ban + Email | Anti SSH Brute Force
-yes_or_no "Install Fail2ban? (Anti SSH Brute Force)" && apt-get install fail2ban -y && yes_or_no "Notify by EMail on Security Event?" && apt install sendmail-bin sendmail
+#yes_or_no "Install Fail2ban? (Anti SSH Brute Force)" && apt-get install fail2ban -y && yes_or_no "Notify by EMail on Security Event?" && apt install sendmail-bin sendmail
+
+# ClamAV + Auto File Scan | Antivirus
+#yes_or_no "Install ClamAV? (File AntiVirus)" && echo "Install #2. AUTO SCAN, AUTO UPDATE SIGNATURES. DO POST UFW SO YOU CAN ADD RULES"
+
+# Disable Unused Filesystems
+#yes_or_no "Disable Unused Filesystems? (Reduce Attack Vectors)" && echo "Install #2"
+
+# Intrusion Detection EMails <Probably not useful YET>
+#yes_or_no "Install Advanced Intrusion Detection (AIDE)?" && sudo apt install aide && 
+
+# Harden Kernel Security
+#yes_or_no "Harden Kernel Security Paramaters?" && echo "Install #4"
+
+# Disable Core Dumps / Logs
+#yes_or_no "Disable Core Dumps? (!!No More Crash Logs!!)" && echo "Install #5"
+
+# SSH 2FA
+yes_or_no "Install SSH 2FA?" && apt install libpam-google-authenticator && echo && echo "----------------------------" && echo "SAVE YOUR BACKUP KEYS" && echo "----------------------------" && echo && google-authenticator && sudo sed -i "s/PermitEmptyPasswords no\b/PermitEmptyPasswords no \nChallengeResponseAuthentication yes/gI" /etc/ssh/sshd_config && sudo sed -i "s/@include common-auth\b/@include common-auth \n \n#Two Factor Authentication with Google Authenticator \nauth    required    pam_google_authenticator.so/gI" /etc/pam.d/sshd && sudo systemctl restart ssh
+
 
 # Remove useless packages
 sudo apt-get autoremove 
